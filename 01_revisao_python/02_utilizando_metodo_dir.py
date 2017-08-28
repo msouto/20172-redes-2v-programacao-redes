35fa666502814a9907bec73c375ffb5b3b01e9e2
Moisés Souto <moises.souto@gmail.com>
2017-08-28 17:38:40 -0300

    Exemplos de objetos bult-in do Python

--- 01_revisao_python/02_utilizando_metodo_dir.py
+++ 01_revisao_python/02_utilizando_metodo_dir.py
@@ -1,4 +1,6 @@
 #Utilizar um metodo para verificar os atributos disponiveis
+
+#exemplos com string
 x = 'spam'
 #print(dir(x))
 print(len(x))
@@ -6,3 +8,29 @@ print(x[0])
 print(x[1:3])
 print(x[-1])
 print(x + ' email')
+
+print(x.find('pa'))
+print(x.upper())
+print(x.isalpha())
+print(x.replace('pa','ca'))
+
+#exemplos com listas
+L = [123,'casa', 3.14]
+#print(dir(L))
+print(len(L))
+print(L[0])
+#print(L*2)
+print(L)
+L.append('bola')
+print(L)
+
+M = [[1,2,3],[4,5,6],[7,8,9]]
+print(M[0])
+col2 = [n[0] for n in M]
+print(col2)
+
+#Exemplo de dicionario
+D = {'com':'ABC','num':123}
+print(D['num'])
+D['cor'] = 'azul'
+print(D['cor'])
