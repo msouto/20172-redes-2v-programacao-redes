4b9bd53f235ebc856e67e3e1570f546671033f31
Moisés Souto <moises.souto@gmail.com>
2017-09-13 17:07:34 -0300

    Manipulando matrizes em listas

---
+++ 01_revisao_python/08_matrizes.py
@@ -0,0 +1,12 @@
+#matrizes
+m = [[1,2,3],[4,5,6],[7,8,9]]
+print(m)
+#imprimir a segunda linha
+print(m[1])
+
+#imprimir o elemento 8
+print(m[2][1])
+
+#imprimir os elemntos 2,5,8
+v=[x[1] for x in m]
+print(v)
