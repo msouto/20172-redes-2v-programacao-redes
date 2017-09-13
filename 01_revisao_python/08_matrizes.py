f177a48c91f325fb4047cfb68ea542c9b852c810
Moisés Souto <moises.souto@gmail.com>
2017-09-13 17:16:10 -0300

    Operações com matrizes

--- 01_revisao_python/08_matrizes.py
+++ 01_revisao_python/08_matrizes.py
@@ -10,3 +10,21 @@ print(m[2][1])
 #imprimir os elemntos 2,5,8
 v=[x[1] for x in m]
 print(v)
+
+#alterar através de indexação e slides
+#alterar o valor da segunda posição
+l=[1,5,6]
+print(l)
+l[1]=9
+print(l)
+
+#alterar dois elementos
+#l[0:2] = ['abc', 25]
+#print(l)
+
+#adicionar um elemento ao final da lista
+l.append('abc')
+print(l)
+#adicionar vários itens ao final da lista
+l.extend([10,11,12])
+print(l)
