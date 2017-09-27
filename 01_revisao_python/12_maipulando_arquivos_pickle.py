8f9edaba8e1ed1481daa61e1759a67bcf1b0937b
Moisés Souto <moises.souto@gmail.com>
2017-09-27 17:04:11 -0300

    Manipulando leitura de arquivo do pickle

--- 01_revisao_python/12_maipulando_arquivos_pickle.py
+++ 01_revisao_python/12_maipulando_arquivos_pickle.py
@@ -5,3 +5,9 @@ d={'nome':'Moisés','disciplina':'programação para redes'}
 arquivo=open('12_arquivo.pkl','wb')
 res=pickle.dump(d,arquivo)
 arquivo.close()
+
+#Leitura
+arquivo=open('12_arquivo.pkl','rb')
+res=pickle.load(arquivo)
+print(res)
+arquivo.close()
