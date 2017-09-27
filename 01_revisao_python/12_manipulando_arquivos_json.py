49efb1a3df920a1ac37eda7fdc861cd8c67ae0b4
Moisés Souto <moises.souto@gmail.com>
2017-09-27 17:26:09 -0300

    Manipulando leitura de arquivos JSON

--- 01_revisao_python/12_manipulando_arquivos_json.py
+++ 01_revisao_python/12_manipulando_arquivos_json.py
@@ -5,3 +5,9 @@ d=dict(nome='moisés',disciplina='Programação para redes')
 arquivo=open('12_arquivo.json','w')
 res=json.dump(d,arquivo,indent=4)
 arquivo.close()
+
+#ler o arquivo JSON
+arquivo=open('12_arquivo.json','r')
+res=json.load(arquivo)
+print(res)
+arquivo.close()
