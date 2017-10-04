e2bdb24ab5ec3ca619e9f0b225002468bcc0e97a
Alexsander <silvioalexsander5@gmail.com>
2017-10-04 17:53:09 -0300

    exercicio 2

--- exercicios_em_sala/exe02/exer02.py
+++ exercicios_em_sala/exe02/exer02.py
@@ -1,18 +1,41 @@
 import sys
 
+def valida_data(data):
+    data= data.split('/')
+    if len(data) == 1 :
+        print('Digite a data separada com "/" (barras)')
+
+    if int(data[0]) < 1 and int(data[0] > 31:
+        print('Insira um dia de 1 a 31')
+
+    if int(data[1]) < 1 and int(data[1]) > 12:
+        print('Insira um mês de 1 a 12')
+    if len(data[2]) != 4:
+
+        print('Insira um ano com 4 digitos')
+    else:
+        return "ok"
+
 def data_por_extenso(texto):
-    meses=['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro', 'outubro','novembro','dezembro']
     data_num = texto.split('/')
+    meses=['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro', 'outubro','novembro','dezembro']
     return data_num[0] + ' de ' + meses[int(data_num[1])-1] + ' de ' + data_num[2]
 
 def main():
-    if len(sys.argv) == 2:
-        print(data_por_extenso(sys.argv[1]))
-    else:
+    
+    if len(sys.argv) == 1:
+        print('Você não passou nenhum argumento!')
+    elif len(sys.argv) > 2:
         print('Voce deve passar apenas um argumento com a data a ser convertida!')
+    else:
+        if valida_data(sys.argv[1]) == "ok"
+            print(data_por_extenso(sys.argv[1]))
+     
+        
 
 if __name__ == '__main__':
-    main()
+  main()
+
 
 #Exercício 02
 #======================================================
