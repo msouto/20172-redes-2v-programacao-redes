39ff1bc25aff087a61d6be197037dad6928d4366
Moisés Souto <moises.souto@gmail.com>
2017-11-08 17:15:03 -0300

    Adicionando exercício em sala 03

---
+++ exercicios_em_sala/exe03/exe03.py
@@ -0,0 +1,1 @@
+#Implemente um cliente multithread em Python, que concete na porta 2000 e envie pelo 5 mensagens em threads diferentes no cliente. As mensagens enviadas devem ser enumeradas, ex: Mensagem da thread 1, Mensagem da thread 2 e etc.)
