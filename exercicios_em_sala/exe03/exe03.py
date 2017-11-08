3f641858a6f564abf7b62ef217103689f6d45dd5
AlisonFSilva <alisonferreira2015.1@outlook.com>
2017-11-08 17:39:06 -0300

    Update exe03.py

--- exercicios_em_sala/exe03/exe03.py
+++ exercicios_em_sala/exe03/exe03.py
@@ -1,1 +1,20 @@
 #Implemente um cliente multithread em Python, que concete na porta 2000 e envie pelo 5 mensagens em threads diferentes no cliente. As mensagens enviadas devem ser enumeradas, ex: Mensagem da thread 1, Mensagem da thread 2 e etc.)
+import socket
+import threading
+
+HOST = socket.gethostbyname('localhost')
+PORT = 2000
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST,PORT))
+
+while True:
+    threading.Thread().start()
+    message = input("Digite mensagem: ")
+    byte_message = message.encode('utf-8')
+    client_socket.send(byte_message)
+
+    data = client_socket.recv(1024)
+    print(data)
+
+tcp_client_socket.close
