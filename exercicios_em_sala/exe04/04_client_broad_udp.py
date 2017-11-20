3f5a3e5de0ab24cb542a002957f795d905836b8e
Thiago <thiagofrdal@gmail.com>
2017-11-20 17:07:03 -0300

    Correção

--- exercicios_em_sala/exe04/04_client_broad_udp.py
+++ exercicios_em_sala/exe04/04_client_broad_udp.py
@@ -4,7 +4,7 @@ serverPort = 12000
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-message = input("Entre com a sting minuscula: ")a
+message = input("Entre com a sting minuscula: ")
 byte_msg = message.encode('utf-8')
 clientSocket.sendto(byte_msg, (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
