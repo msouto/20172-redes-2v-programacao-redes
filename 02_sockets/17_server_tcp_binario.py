252ba5d194cb5f150bde214224efce42a01353ed
Moisés Souto <moises.souto@gmail.com>
2017-11-20 17:58:12 -0300

    Exemplo de servidor TCP/IP multithread com transferência binária

---
+++ 02_sockets/17_server_tcp_binario.py
@@ -0,0 +1,47 @@
+import socket
+from threading import Thread
+from SocketServer import ThreadingMixIn
+
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+BUFFER_SIZE = 1024
+
+class ClientThread(Thread):
+
+    def __init__(self,ip,port,sock):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.sock = sock
+        print("Nova thread inicializada para "+ip+":"+str(port))
+
+    def run(self):
+        filename='mytext.txt'
+        f = open(filename,'rb')
+        while True:
+            l = f.read(BUFFER_SIZE)
+            while(l):
+                self.sock.send(l)
+                l = f.read(BUFFER_SIZE)
+            if not l:
+                f.close()
+                self.sock.close()
+                break
+
+
+tcpsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+tcpsock = setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock = bind((TCP_IP,TCP_PORT))
+threads = []
+
+while True:
+    tcpsock.listen(5)
+    print("Aguardando conexões...")
+    (conn,(ip,port)) = tcpsock.accept()
+    print("Conexão recebida de:"+ip+":"+str(port))
+    newthread = ClientThread(ip,port,conn)
+    newthread.start()
+    threads.append(newthread)
+
+for t in threads:
+    t.join()
